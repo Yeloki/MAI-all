e2665bb44f73089abfeb33f7e8380c59a2bd5a4d
Yeloki <gtgtr3000@gmail.com>
2021-12-23 02:00:12 +0300

    upd

--- 1 semester/computing systems/13 lab/main.c
+++ 1 semester/computing systems/13 lab/main.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 
-int get_from_0(char c) {
-  int t = c / 100;
-  if (65 >= t && t <= 90)
-    return t - 65;
-  if (97 >= t && t <= 122)
-    return t - 97;
+int is_vowel(char c) {
+  for (int i = 0; i < 12; ++i)
+    if ("aAeEiIoOuUyY"[i] == c)
+      return 1;
+  return 0;
+}
+
+int get_vowel_from_0(char c) {
+  for (int i = 0; i < 12; ++i)
+    if ("aeiouyAEIOUY"[i] == c)
+      return i % 6;
   return -1;
 }
 
+int is_char(char c) {
+  return 65 <= c && c <= 90 || 97 <= c && c <= 122;
+}
+
 int main() {
-  int v[28];
+  int v[6] = {0, 0, 0, 0, 0, 0};
   char c;
-  while ((c = getchar()) != EOF) {
-    if (get_from_0(c) == -1)
-      continue;
-    ++v[get_from_0(c)];
-  }
-  for (int i = 0; i < 28; ++i) {
-    if (v[i] > 1) {
-      printf("YES");
-      return 0;
+  int cntr = 0;
+  while ((c = getchar()) != EOF || c == '-') {
+    if (!is_char(c)) {
+      int flag = 0;
+      for (int i = 0; i < 6; ++i) {
+        if (v[i] >= 1) {
+          ++flag;
+        }
+        v[i] = 0;
+      }
+      if (flag == 1)
+        ++cntr;
+    } else {
+      if (is_vowel(c))
+        ++v[get_vowel_from_0(c)];
     }
   }
-  printf("NO");
+  printf(cntr ? "YES" : "NO");
+
   return 0;
 }
